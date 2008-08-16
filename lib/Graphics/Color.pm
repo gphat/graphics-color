dc7a7f8941ec05418680dce7f14f3ed9151eaa02
Cory G Watson <gphat@onemogin.com>
2008-08-16 00:06:04 +0000

    Version bump

--- lib/Graphics/Color.pm
+++ lib/Graphics/Color.pm
@@ -5,7 +5,7 @@ use Moose::Util::TypeConstraints;
 with qw(MooseX::Clone Graphics::Color::Equal);
 
 our $AUTHORITY = 'cpan:GPHAT';
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 subtype 'Graphics::Color::Number360OrLess'
     => as 'Num',
