c0336f8b246749a3f7a615b20fd4e534f746b452
Cory G Watson <gphat@onemogin.com>
2008-09-09 21:31:58 +0000

    Remove moose coverage dep

--- lib/Graphics/Color.pm
+++ lib/Graphics/Color.pm
@@ -7,7 +7,7 @@ with qw(MooseX::Clone Graphics::Color::Equal);
 with Storage (format => 'JSON', io => 'File');
 
 our $AUTHORITY = 'cpan:GPHAT';
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 subtype 'Graphics::Color::Number360OrLess'
     => as 'Num',
